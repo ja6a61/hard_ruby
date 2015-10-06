eb0e07d5bbb336c09088ca5c242964de6cce2dfb
Jason Angus <ja.6a61@gmail.com>
2015-10-07 02:14:10 +0800

    First commit

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+# ex11.rb
+# Tue 06 Oct 2015
+# Exercise 11: Asking Questions
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So you are #{age} years old, #{height} tall and #{weight} heavy."
+
---
+++ ex3.rb
@@ -0,0 +1,7 @@
+# ex3.rb
+puts "What is 3 + 2? #{3 + 2}"
+puts "Is 5 > -2? #{5 > -2}"
+puts "Is 5 < -2? #{5 < -2}"
+puts "What is 12 / 2.32?"
+puts "#{12 / 2.32}"
+
