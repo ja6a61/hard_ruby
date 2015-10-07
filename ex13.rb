85fb21130ea84c483724b49966f4fe3f1df68b30
Jason Angus <ja.6a61@gmail.com>
2015-10-07 22:23:04 +0800

    Added .gitignore file.

---
+++ ex13.rb
@@ -0,0 +1,10 @@
+# ex13.rb
+# Wed 07 Oct 2015
+#
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
