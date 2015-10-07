59e867df23ed4d24189c97adc9ab1154932ece11
Jason Angus <ja.6a61@gmail.com>
2015-10-08 04:46:42 +0800

    Completed ex15.

---
+++ ex15.rb
@@ -0,0 +1,20 @@
+# ex15.rb
+# Exercise 15: Reading Files
+# Thu 08 Oct 2015
+#
+
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "Here's your file #{filename}:"
+print txt.read
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
+
+
