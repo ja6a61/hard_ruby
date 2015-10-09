354ee513c1ff68de1faeffb7ed470f872de0f62b
Jason Angus <ja.6a61@gmail.com>
2015-10-09 16:39:43 +0800

    Going for a consistant quote style

--- ex16.rb
+++ ex16.rb
@@ -6,24 +6,24 @@
 filename = ARGV.first
 
 puts "We're gonna erase #{filename}"
-puts "If you don't wanna do that, hit CTRL-C"
-puts "If you do want that, hit ENTER"
+puts 'If you don\'t wanna do that, hit CTRL-C'
+puts 'If you do want that, hit ENTER'
 
 $stdin.gets
 
-puts "Opening the file..."
+puts 'Opening the file...'
 target = open(filename, 'w')
 
-puts "Truncating the file. Goodbye file contents!!"
+puts 'Truncating the file. Goodbye file contents!!'
 target.truncate(0)
 
-puts "Now I'm gonna ask you for three lines."
+puts 'Now I\'m gonna ask you for three lines.'
 
-print "Line 1: "
+print 'Line 1: '
 line1 = $stdin.gets.chomp
-print "Line 2: "
+print 'Line 2: '
 line2 = $stdin.gets.chomp
-print "Line 3: "
+print 'Line 3: '
 line3 = $stdin.gets.chomp
 
 puts "Gonna write these lines to the file just now..."
