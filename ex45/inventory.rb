b40e416cf0c282d042fa12e98818c4007bca362e
Jason Angus <ja.6a61@gmail.com>
2015-10-29 01:23:15 +0800

    text game

---
+++ ex45/inventory.rb
@@ -0,0 +1,12 @@
+# inventory.rb
+# module for inventory related items
+# last edit:
+# Thu 29 Oct 2015
+#
+
+module Inventory
+
+food = ['water', 'apples', 'bananas', 'bread', 'cheese', 'wine', 'paw paw',
+        'mango', 'avocado', 'rice']
+
+end
